19ccaba13915f1dc7a4274135b3a3579ca0fd557
Jaden <624133845@qq.com>
2019-07-02 16:12:59 +0800

    根据路由动态修改页面标题

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +17,39 @@ export class AppComponent {
   ngAfterViewInit - 组件相应的视图初始化之后调用
   ngAfterViewChecked - 组件每次检查视图时调用
   ngOnDestroy - 指令销毁前调用 **/
+
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.routerChange();
+  }
+
+  /** 监听路由变化 */
+  routerChange() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),  // 筛选原始的Observable：this.router.events
+        map(() => this.activatedRoute),
+        map(route => {
+          while (route.firstChild) {
+            route = route.firstChild;
+          }
+          return route;
+        }),
+        mergeMap(route => route.data)
+      )
+      .subscribe((event) => {
+        /** 根据路由动态修改页面标题 */
+        if (event["title"]) {
+          this.titleService.setTitle(event["title"])
+        }
+      });
+  }
 }
